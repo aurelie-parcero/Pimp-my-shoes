033108ff9629ad73920344007f0a7c989002c6b7
aurelie-p <aurelie-p@users.noreply.github.com>
2020-02-26 16:51:26 +0100

    ajout debut footer

---
+++ resources/views/footer-mobile.blade.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="container-fluid">
+        <div class="container border_footer">
+        </div>
+
+        <div class="social_media_mobile col-sm-8" >
+            <i class="fab fa-pinterest-p"><a href="http://www.pinterest.com"></a></i>
+            <i class="fab fa-linkedin-in"><a href="http://www.linkedin.fr"></a></i>
+            <i class="fab fa-facebook-f"><a href="http://www.facebook.fr"></a></i>
+            <i class="fab fa-instagram"><a href="http://www.instagram.com"></a></i>
+            <i class="fab fa-twitter"><a href="http://www.twitter.com"></a></i>
+        </div>
+
+        <div>
+            <div>
+
+            </div>
+
+            <div class="mentions_bas_page">
+                <p>Mentions Légales</p>
+                <p>CGV</p>
+                <p>Protections des données</p>
+            </div>
+
+        </div>
+
+    </div>
+</footer>
+
+
+<script crossorigin="anonymous" src="https://kit.fontawesome.com/b0864c5c2c.js"></script>
+
+</body>
+</html>
