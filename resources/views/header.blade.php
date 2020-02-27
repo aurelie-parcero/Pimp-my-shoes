c7151aeea17367cae7724c4d0155d526f92dcbc7
Adil D.Dark <adil.dahmani-darkaoui@le-campus-numerique.fr>
2020-02-27 16:30:34 +0100

    preparing banner style

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,19 +1,7 @@
-<html>
 <header>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo asset('style.css')?>" type="text/css">
-    <title>Document</title>
-</header>
-<body>
-
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark banner">
     <!-- Brand -->
-    <a class="navbar-brand" href="#"><img src="{{ asset('element_ressource/logo/header_mobile_police_blanche.png') }}"
+    <a class="navbar-brand" href="#"><img src="{{ asset('storage/element_ressource/logo/header_mobile_police_blanche.png') }}"
                                           style="width:150px;"/></a>
 
 
@@ -37,24 +25,12 @@
     <div class="collapse navbar-collapse text-right" id="collapsibleNavbar">
         <ul class="navbar-nav ml-auto flex-nowrap d-md-none"style="flex-direction: column">
             <li class="nav-item">
-                <a href="#" class="nav-link m-2 menu-item nav-active">Link1</a>
+                <a href="#" class="nav-link m-2 menu-item nav-active">Femme</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link m-2 menu-item">Link2</a>
+                <a href="#" class="nav-link m-2 menu-item">Homme</a>
             </li>
         </ul>
     </div>
-
 </nav>
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+</header>
