3558301e1289be83ac75cdd72e6964bdb5038e8f
Adil D.Dark <adil.dahmani-darkaoui@le-campus-numerique.fr>
2020-02-27 15:01:18 +0100

    header à jour

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 
-<<<<<<< HEAD
+
 @include('header')
 
-=======
+
     <body>
->>>>>>> 066a923eb9dd27ace96c3835eadfb94660a66ad3
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -32,13 +32,8 @@
 
                     Proin tristique augue imperdiet congue fermentum. Ut vitae commodo diam. Pellentesque ultricies volutpat turpis sit amet hendrerit. Pellentesque tincidunt pretium ligula, quis faucibus ipsum placerat ac. Donec euismod imperdiet mi et euismod. Nulla viverra urna vitae risus tempus, vitae ornare nibh fringilla. Integer et augue leo.
 
-                    Cras faucibus quis orci sit amet gravida. Proin elementum magna non arcu venenatis, id imperdiet dui commodo. Proin mattis iaculis dui viverra mattis. Pellentesque mollis ornare tellus, vel condimentum purus ornare vel. Cras mi arcu, dictum id ultrices vel, egestas ac quam. Morbi nulla metus, dapibus in egestas a, tempus et ipsum. Aenean sodales ante a nulla tempor aliquet. Phasellus suscipit magna in suscipit lobortis. Morbi ut leo eget arcu mollis tempus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas quis ligula ante. Praesent in ex non massa scelerisque viverra quis ut nibh. Etiam eleifend odio eros, eu imperdiet mauris mollis at.
-
-                    Fusce nec justo leo. Sed malesuada sapien non interdum dignissim. Etiam vehicula, risus et consectetur dapibus, mauris sem rutrum nibh, quis finibus lorem quam vitae leo. Integer vitae sem tortor. Morbi mollis luctus velit vel hendrerit. Proin in metus elit. Vivamus id volutpat turpis. Nullam ac ligula sagittis, faucibus elit ut, tristique enim.
-
-                    Praesent consectetur eleifend dui non facilisis. Sed eget mauris vel metus laoreet elementum. Praesent sollicitudin, lectus eget laoreet accumsan, dui arcu tempor lacus, et bibendum elit nisl at lectus. Proin vel nisi risus. Suspendisse et risus et urna tincidunt malesuada. Nulla cursus ipsum ac tellus efficitur, a sagittis risus tincidunt. Sed ac leo nec erat accumsan mattis at non purus. Aliquam vestibulum justo sapien, maximus sodales velit ornare sed. Mauris lobortis ullamcorper nulla sit amet molestie. Aliquam dictum in nibh a bibendum. Nunc luctus felis ut ultrices pharetra. Sed tempor imperdiet quam eget vulputate. Phasellus ultricies venenatis urna. Praesent varius felis eget velit consequat dictum. In metus purus, hendrerit eget odio et, porttitor ultricies lacus.
-
-                    Nunc gravida augue eget suscipit semper.             </p>
+                    Cras faucibus quis orci sit amet gravida. Proin elementum magna non arcu venenatis, id imperdiet dui commodo. Proin mattis iaculis dui viverra mattis.
+                </p>
 
                 @include('button')
 
