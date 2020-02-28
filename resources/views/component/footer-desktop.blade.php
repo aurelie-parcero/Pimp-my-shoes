ea785271d21216731a56cb4fea897eb75dbea864
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2020-02-28 17:00:35 +0100

    mise a jour footer

--- resources/views/component/footer-desktop.blade.php
+++ resources/views/component/footer-desktop.blade.php
@@ -1,34 +1,34 @@
 <footer class="container-fluid footer-desktop">
 
-    <div class="container border_footer">
+    <div class=" border_footer">
     </div>
 
-    <div id="icone_footer" class="footer-desk-yellow container">
-        <div id="icone_info_footer" class="footer-desktop-info item">
-            <div class="col-6">
-                <img alt="livraison gratuite" class="icone-footer-desk-truck"
-                     src="{{asset('icones/footer_truck_ylw.png')}}"/>
-                <p class="text-footer-desk">Livraison gratuite dès 59€</p>
+    <div class="footer-desk-yellow">
+            <div class="footer-desktop-info col-6">
+                <div class=" icone-footer-desk">
+                    <img alt="livraison gratuite" class="icone-footer-desk-truck"
+                         src="{{asset('icones/footer_truck_ylw.png')}}"/>
+                    <p class="text-footer-desk">Livraison gratuite dès 59€</p>
+                </div>
+                <div class=" icone-footer-desk">
+                    <img alt="paiement sécurisé" class="icone-footer-desk-lock"
+                         src="{{asset('icones/footer_lock_ylw.png')}}"/>
+                    <p class="text-footer-desk">Paiement 100% sécurisé</p>
+                </div>
+                <div class=" icone-footer-desk">
+                    <img alt="livraison rapide" class="icone-footer-desk-clock"
+                         src="{{asset('icones/footer_clock_ylw.png')}}"/>
+                    <p class="text-footer-desk">Livré en 5 à 7 jours</p>
+                </div>
             </div>
-            <div class="col-6">
-                <img alt="paiement sécurisé" class="icone-footer-desk-lock"
-                     src="{{asset('icones/footer_lock_ylw.png')}}"/>
-                <p class="text-footer-desk">Paiement 100% sécurisé</p>
-            </div>
-            <div class="col-6">
-                <img alt="livraison rapide" class="icone-footer-desk-clock"
-                     src="{{asset('icones/footer_clock_ylw.png')}}"/>
-                <p class="text-footer-desk">Livré en 5 à 7 jours</p>
+            <div id="icone-sociale_footer" class="col-4 social_media_desktop">
+                <i class="fab fa-pinterest-p"><a href="http://www.pinterest.com"></a></i>
+                <i class="fab fa-linkedin-in"><a href="http://www.linkedin.fr"></a></i>
+                <i class="fab fa-facebook-f"><a href="http://www.facebook.fr"></a></i>
+                <i class="fab fa-instagram"><a href="http://www.instagram.com"></a></i>
+                <i class="fab fa-twitter"><a href="http://www.twitter.com"></a></i>
             </div>
 
-        </div>
-        <div id="icone-sociale_footer" class="col-6 social_media_desktop">
-           <div class="icone-pinterest"> <i class="fab fa-pinterest-p"><a href="http://www.pinterest.com"></a></i></div>
-            <i class="fab fa-linkedin-in"><a href="http://www.linkedin.fr"></a></i>
-            <i class="fab fa-facebook-f"><a href="http://www.facebook.fr"></a></i>
-            <i class="fab fa-instagram"><a href="http://www.instagram.com"></a></i>
-            <i class="fab fa-twitter"><a href="http://www.twitter.com"></a></i>
-        </div>
     </div>
 
     <div class="footer-blue">
