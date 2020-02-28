f4ade8bac774690464c758b7cb0b5f1b48c4c55a
Matthias <matthias.codvelle@le-campus-numerique.fr>
2020-02-28 13:58:33 +0100

    architecture bonne

--- resources/views/page/accueil.blade.php
+++ resources/views/page/accueil.blade.php
@@ -9,32 +9,32 @@
     <div class="container">
 
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/stan.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/stan.png')}}"/>
             <p>A partir de </p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/converse.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/converse.png')}}"/>
             <p>A partir de </p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/escarpin.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/escarpin.png')}}"/>
             <p>A partir de </p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/espadrille.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/espadrille.png')}}"/>
             <p>A partir de </p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/femmeboot.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/femmeboot.png')}}"/>
             <p>A partir de }</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/chaussurehomme.png"/>
+            <img class="img-produit-catalogue" src="{{asset('images/femmeboot.png')}}"/>
             <p>A partir de</p>
             <div>Personnaliser</div>
         </div>
