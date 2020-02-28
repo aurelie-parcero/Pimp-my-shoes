fcce77e4aee5e9b7ea0396e0a514cc177a74f120
Matthias <matthias.codvelle@le-campus-numerique.fr>
2020-02-28 10:01:24 +0100

    route crée

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,15 @@ Route::get('/', function () {
     return view('page/accueil');
 });
 
+Route::get('/', function () {
+    return view('page/contact');
+});
+
+Route::get('/', function () {
+    return view('page/fiche-produit');
+});
+
+Route::get('/', function () {
+    return view('page/panier');
+});
+
