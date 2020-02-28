5d7b72326017057db9a5598c3189f33cdf078b17
aurelie-p <aurelie-p@users.noreply.github.com>
2020-02-28 11:03:14 +0100

    ajout image png

--- resources/views/page/accueil.blade.php
+++ resources/views/page/accueil.blade.php
@@ -9,36 +9,36 @@
     <div class="container">
 
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/stan.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/stan.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/converse.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/converse.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/escarpin.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/escarpin.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/espadrille.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/espadrille.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/femmeboot.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/femmeboot.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
         <div class="block-produit">
-            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/chaussurehomme.png" />
+            <img class="img-produit-catalogue" src="storage/app/public/element_ressource/images/chaussurehomme.png"/>
             <p>A partir de {{$prix}}</p>
             <div>Personnaliser</div>
         </div>
 
     </div>
 
-                    @endsection
+@endsection
