4784873f223aa2cd27d7e913afe4dd33ae294961
Matthias <matthias.codvelle@le-campus-numerique.fr>
2020-03-02 10:45:17 +0100

    style boutton fiche-produit

--- resources/views/page/fiche-produit.blade.php
+++ resources/views/page/fiche-produit.blade.php
@@ -5,21 +5,22 @@
 @section('titre', '')
 
 @section('contenu')
-    <div class="container">
-        <img src="{{asset('')}}" alt="">
+    <div class="fpContainer">
+        <img src="{{asset('images/stanwhite.jpg')}}" alt="" class="fpImageDuProduit">
         <div class="fpImages">
-            <img src="{{asset('')}}" alt="">
-            <img src="{{asset('')}}" alt="">
-            <img src="{{asset('')}}" alt=""><br /><br />
-        </div>
+            <img src="{{asset('images/stanwhite.jpg')}}" alt="" class="fpImagesPersonalisation">
+            <img src="{{asset('images/stanwhite.jpg')}}" alt="" class="fpImagesPersonalisation">
+            <img src="{{asset('images/stanwhite.jpg')}}" alt="" class="fpImagesPersonalisation">
+        </div><br /><br />
         <div class="fpButton">
             <button name="Personaliser" type="button">Personaliser</button>
             <button name="Produit" type="button">Produit</button>
-            <button name="Continuer" type="button">Continuer</button>
+            <button name="Continuer" type="button" class="fpOrangeContinuer">Continuer</button>
         </div><br /><br />
         <div class="fpChoixTaille">
             <p>Choisir la taille <label for="option"></label>
             <select name="option" id="option">
+                <option value="">Option</option>
                 <option value="36">36</option>
                 <option value="37">37</option>
                 <option value="37.5">37.5</option>
