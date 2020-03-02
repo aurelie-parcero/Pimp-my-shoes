fd024836616cd26c5a25d5481e0c1231fac2dfa3
aurelie-p <aurelie-p@users.noreply.github.com>
2020-03-02 14:56:20 +0100

    ajout view/controlelr

--- app/Http/Controllers/ControllerAccueil.php
+++ app/Http/Controllers/ControllerAccueil.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class ControllerAccueil extends Controller
 {
     public function viewAccueil() {
-        return view('page/accueil');
+
+        $catalogue = DB::select('select * from catalogue');
+        dump($catalogue);
+        return view('page/accueil', ['catalogue' => $catalogue]);
     }
 }
--- app/Http/Controllers/ControllerFicheProduit.php
+++ app/Http/Controllers/ControllerFicheProduit.php
@@ -7,6 +7,7 @@ use App\Http\Controllers;
 class ControllerFicheProduit extends Controller
 {
     public function viewFicheProduit() {
+        $this->connexionBDD();
         return view('page/fiche-produit');
     }
 }
