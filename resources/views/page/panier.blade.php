4b88c122a10e8d962a1d47c6b0ff023949a076f6
aurelie-p <aurelie-p@users.noreply.github.com>
2020-03-02 11:41:19 +0100

    avancement page panier

--- resources/views/page/panier.blade.php
+++ resources/views/page/panier.blade.php
@@ -7,52 +7,65 @@
 @section('contenu')
     <!-- Si le panier est vide -->
     <section id="panier">
-        <p>
-            Votre panier est vide.
-        </p>
+        <div class="panier-vide">
+            <p>
+                Votre panier est vide.
+            </p>
 
-        <button type="button">Continuer mes achats</button>
-
+            <button id="panier-achat" type="button">Continuer mes achats</button>
+        </div>
 
         <!-- Si le panier contient un article, les div suivantes doivent être remplies -->
-        <div class="container">
-            <h2>Ref produit</h2>
-            <img src="" alt=""/>
-        </div>
-        <div>
-            <p>Taille</p>
-            <p>/la taille/</p>
-        </div>
-        <div>
-            <p>Quantité</p>
-            <p>//la quantité</p>
-            <div>
-                <button type="button"><i class="fas fa-minus"></i></button>
-                <button type="button"><i class="fas fa-plus"></i></button>
-            </div>
-        </div>
-        <div>
-            <a href=""><i class="fas fa-cog"></i></a>
-        </div>
-        <div>
-            <a href=""><i class="fas fa-trash"></i></a>
-        </div>
-        <div>
-            <div>
-                <p>Prix unitaire</p>
-                <div>//Prix// € TTC</div>
-            </div>
-            <div>
-                <p>Composition</p>
-                <p>// composition //</p>
-            </div>
-        </div>
-        <div>
-            <p> Sous-total</p>
-            <div>// sous-total //</div>
-        </div>
+        <div class="container panier-contenu">
 
-        <button type="submit">Valider</button>
+            <div class="row">
+
+                <div class="col-sm 12 col-lg-12">
+                    <h2>Ref produit</h2>
+                    <img src="" alt=""/>
+                </div>
+
+                <div class="info-panier-1 col-sm-12 col-lg-12">
+                    <div class="col-sm-3">
+                        <p>Taille</p>
+                        <p>/la taille/</p>
+                    </div>
+                    <div class="col-sm-3 quantite">
+                        <p>Quantité</p>
+                        <p class="frame-ylw">//la quantité</p>
+                        <div class="bouton-quantite">
+                            <button type="button"><i class="fas fa-minus"></i></button>
+                            <button type="button"><i class="fas fa-plus"></i></button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href=""><i class="fas fa-cog"></i></a>
+                    </div>
+                    <div class="col-sm-3">
+                        <a href=""><i class="fas fa-trash"></i></a>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="row panier-prix">
+                <div class="col-sm-12 case-prix">
+                    <p>Prix unitaire</p>
+                    <div class="frame-ylw">//Prix// € TTC</div>
+                </div>
+                <div class="panier-compo">
+                    <p>Composition</p>
+                    <p>// composition //</p>
+                </div>
+            </div>
+            <div>
+                <p> Sous-total</p>
+                <div>// sous-total //</div>
+            </div>
+
+            <button type="submit" class="frame-ylw button-ylw">Valider</button>
+        </div>
     </section>
 
 @endsection
