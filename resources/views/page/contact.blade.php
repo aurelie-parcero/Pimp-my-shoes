53ac66e4093bc2615a2750fe0f88c2f7f6054154
aurelie-p <aurelie-p@users.noreply.github.com>
2020-04-09 12:05:28 +0200

    ajout methode validate js

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -7,21 +7,21 @@
 @section('contenu')
 
     <div class="Cformulaire">
-        <form action="" method="post">
-            <label>PRÉNOM<br /><input type="text" name="prénom" /></label><br />
-            <label>NOM<br /><input type="text" name="prénom" /></label><br />
-            <label>EMAIL<br /><input type="email" name="prénom" /></label><br /><br />
-            <label for="Raison">Raison</label>
-                <select name="Raison" id="Raison">
-                    <option value="raison1">raison1</option>
-                    <option value="raison2">raison2</option>
-                    <option value="raison3">raison3</option>
-                    <option value="raison4">raison4</option>
-                    <option value="raison5">raison5</option>
+        <form action="" method="post" id="contact-form">
+            <label>PRÉNOM<br /><input type="text" name="firstName" required></label><br />
+            <label>NOM<br /><input type="text" name="lastName" required></label><br />
+            <label>EMAIL<br /><input type="email" name="email" placeholder="john.doe@gmail.com" required></label><br /><br />
+            <label for="reason">Raison</label>
+                <select name="reason" id="reason">
+                    <option value="reason1">Une commande</option>
+                    <option value="reason2">La livraison</option>
+                    <option value="reason3">Un retour</option>
+                    <option value="reason4">Un article</option>
+                    <option value="reason5">Autres</option>
                 </select><br /><br />
-            <label for="Message">Message</label><br />
-            <textarea name="Message" id="Message" rows="6" cols="45"></textarea><br /><br />
-            <button class="CEnvoyer" type="submit">Envoyer</button><br />
+            <label for="message">Message</label><br />
+            <textarea name="message" id="message" rows="6" cols="45"></textarea><br /><br />
+            <button class="CEnvoyer" type="submit" disabled>Envoyer</button><br />
         </form>
     </div>
 
